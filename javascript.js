528f18472d45d7a99ceb417f7ed5752a971c5567
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-08-27 17:51:27 +0600

    Call history section complet

--- javascript.js
+++ javascript.js
@@ -10,12 +10,8 @@ function countIncrease(id){
         event.preventDefault();
 
         if(event.type === 'click'){
-        console.log("Your tricked worked");
         heartCount++;
          }
-    
-        console.log(heartCount);
-
         document.getElementById('counting-box').innerText = heartCount;
 
     })
@@ -35,38 +31,126 @@ countIncrease('card-seven');
 countIncrease('card-eight');
 countIncrease('card-nine');
 
-// Calling allert & coin reduction function 
+
+// Copy Counting function
+
+ let copyCount = 0;
+
+function copycounting(id){
+
+    document.getElementById(id).addEventListener('click', function(event){
+
+        event.preventDefault();
+
+        if(event.type === 'click'){
+        copyCount++;
+         }
+
+        document.getElementById('copy').innerText = copyCount;
+
+    })
+
+}
+
+// Copy counter function calling
+
+copycounting('copy-1');
+copycounting('copy-2');
+copycounting('copy-3');
+copycounting('copy-4');
+copycounting('copy-5');
+copycounting('copy-6');
+copycounting('copy-7');
+copycounting('copy-8');
+copycounting('copy-9');
 
 
-function callAlertAndCoinReduction(id){
+
+
+// Function for adding a new section
+
+
+let getParent = document.getElementById('parent');
+
+function addCardToList(cardTextId, cardNumberId) {
+  
+    let getTextCard = document.getElementById(cardTextId).innerText;
+    let getNumberCard = document.getElementById(cardNumberId).innerText;
+
+  // Get current time as string
+    let datainfo = new Date().toTimeString();
+
+  // Create a new section
+    let newsection = document.createElement('section');
+
+  // Add HTML content dynamically using template literals
+    newsection.innerHTML = `
+    <section class="call-list flex items-center justify-between p-3 shadow-xl mx-2">
+      <div class="service-name-number">
+        <h1 class="font-bold text-sm">${getTextCard}</h1>
+        <p>${getNumberCard}</p>
+      </div>
+      <div class="time text-xs text-right">${datainfo}</div>    
+    </section>
+    `;
+
+  // Append the new section to the parent container
+    getParent.style.display = "block";
+    getParent.appendChild(newsection);
+
+}
+
+// Calling allert & coin reduction function and calling a funtion to add new section
+
+function callAlertAndCoinReduction(id, cardTextId, cardNumberId){
 
     document.getElementById(id).addEventListener('click', function(event){
 
         let coin = document.getElementById('coin-box').innerText;
         let cointNumber = parseInt(coin);
+
+        let textfield = document.getElementById(cardTextId).innerText;
+        let numberfield = document.getElementById(cardNumberId).innerText;
     
         if(event.type === 'click'){
 
-            console.log(alert("You are about to make a call"));
+            console.log(alert(`You are calling to ${textfield}-${numberfield}`));
             let remaingCoin = cointNumber - 20;
             cointNumber = remaingCoin;
+
         }
 
         document.getElementById('coin-box').innerText = cointNumber;
+        addCardToList(cardTextId, cardNumberId);
 
+        
     })
-
 
 }
 
 // Calling from every card will show an alert and deduct coin
 
-callAlertAndCoinReduction('card-one-calling');
-callAlertAndCoinReduction('card-two-calling');
-callAlertAndCoinReduction('card-three-calling');
-callAlertAndCoinReduction('card-four-calling');
-callAlertAndCoinReduction('card-five-calling');
-callAlertAndCoinReduction('card-six-calling');
-callAlertAndCoinReduction('card-seven-calling');
-callAlertAndCoinReduction('card-eight-calling');
-callAlertAndCoinReduction('card-nine-calling');
+
+callAlertAndCoinReduction('card-one-calling', 'card-one-text', 'card-one-number');
+callAlertAndCoinReduction('card-two-calling', 'card-two-text', 'card-two-number');
+callAlertAndCoinReduction('card-three-calling', 'card-three-text', 'card-three-number');
+callAlertAndCoinReduction('card-four-calling', 'card-four-text', 'card-four-number');
+callAlertAndCoinReduction('card-five-calling', 'card-five-text', 'card-five-number');
+callAlertAndCoinReduction('card-six-calling', 'card-six-text', 'card-six-number');
+callAlertAndCoinReduction('card-seven-calling', 'card-seven-text', 'card-seven-number');
+callAlertAndCoinReduction('card-eight-calling', 'card-eight-text', 'card-eight-number');
+callAlertAndCoinReduction('card-nine-calling', 'card-nine-text', 'card-nine-number');
+
+
+// Clearing all the call history
+
+document.getElementById('clear').addEventListener('click', function(){
+
+    let parentDisplay = document.getElementById('parent');
+
+    parentDisplay.innerHTML = "";  
+    parentDisplay.style.display = "none"
+
+
+})
+
